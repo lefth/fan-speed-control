22320dcc06a3b2e91c94c809a3293b4bdb40aeb7
lefth <github@zwell.net>
2018-03-04 01:02:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# fan-speed-control
+Arduino speed control for a four-wire desktop computer fan. Good for magnetic stirrers.
+
+## Fan controls
+Four-wire computer fans have speed controlled by PWM applied to one of the
+wires. The wire is normally pulled up to 5-12 V, and to reduce the fan speed,
+we should pull it down. Pulling it down all the time would tell the fan to turn
+at its lowest speed. Leaving it high lets the fan run at max speed. If we use
+PWM to pull it down some of the time, the duty cycle will determine the fan's
+speed. Note that you must use a transistor to pull the connection down, as the
+voltage sometimes goes much higher than an Anduino port can handle.
+
+The fan may not be able to handle very high frequency PWM, so Arduino is
+advantageous over a faster dedicated PWM controller.
+
+## Usage
+My circuit has the optional feature of using a momentary push button to change
+whether the fan is on. Connect one end of the button to Arduino and the other
+end to ground, so the button press pulls the pin down. If you don't need this,
+you can hook the fan up to a 12 V power supply directly.
+
+I am using a 10 k potentiometer (as a voltage divider) to control the speed.
+Measure voltage from the pot's middle pin. If you want to use some other logic
+to determine the desired speed, rewrite the `getChosenSpeed` function.
+
+My circuit uses two 2222A transistors, but you could use almost any transistor.
+
+I used a 470 ohm resistor to limit current between the fan's control line and
+the transistor collector, but I don't know whether this is necessary.
+
+## Use for magnetic stirrers
+Magnetic stirrers are very easy to make with a computer fan, but people
+typically control their speed by limiting their voltage with a variable
+resistor. This works badly because they may not have enough torque to get
+started until they're at nearly full power. (Good luck trying to stir a thick
+liquid.) Using the built in speed control doesn't have that problem. However,
+operation is still limited by the lowest speed. My fan's minimum speed is
+probably still too fast to stir something very thick like honey.
+
+## More info about 4-wire fans
+I learned about the speed control in this video:
+https://www.youtube.com/watch?v=gKHww3qJbs8
---
+++ fan-speed-control.ino
@@ -0,0 +1,126 @@
+/*
+MIT License
+
+Copyright (c) 2018 Dan Zwell
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+const int dialPin = A0;     // The middle pin of a potentiometer voltage divider
+const int buttonPin = 12;   // Button for turning the fan on and off
+const int powerPin = 11;    // For controlling whether the fan gets power.
+const int outputPin = 10;   // PWM output, to determine how fast the fan spins.
+						    // The fan speed wire is pulled up. Pull it down to
+						    // ground to set the speed to the lowest level. And
+						    // use PWM (pulling it down to ground) to change the
+						    // speed. In this circuit, setting the output HIGH
+						    // means the fan's wire is pulled down to ground by a
+						    // NPN transistor. Use something like a 470 ohm
+						    // resistor to limit current. You MUST use a
+						    // transistor, since the pulled-up voltage goes to 12V.
+const int ledPin = 13;      // No need to hook up anything if you're happy to look
+                            // directly at the light on the board.
+
+void setup() {
+  pinMode(dialPin, INPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(outputPin, OUTPUT);
+  pinMode(powerPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+// Treat a momentary button as a toggle, detecting each separate press
+// and toggling the state. And ignore noise.
+bool getToggleState()
+{
+  static bool isOn = true;
+  static bool wasPressed = false;
+  const int arrayLength = 40;
+  static bool historyBuffer[arrayLength];
+  static unsigned int index = 0;
+
+  bool _buttonPressed = !digitalRead(buttonPin);
+  historyBuffer[index = (index + 1) % arrayLength] = _buttonPressed;
+
+  // Only consider the button pressed if it has been down for 90% of the
+  // remembered consecutive frames, and only consider it NOT pressed if it's
+  // been it's been up for 90% of the remembered consecutive frames. Otherwise,
+  // assume the previous value is correct. The strict difference between
+  // "pressed" and "not pressed" will reduce flicker.
+  int pressedInMemory = 0;
+  for (int i=0; i<arrayLength; ++i)
+    if (historyBuffer[i])
+      ++pressedInMemory;
+
+  bool buttonPressed;
+  
+  // NOTE: adjust the 10% and 90% ratios if you have an unreliable button
+  if (pressedInMemory / (double)arrayLength >= 0.9)
+    buttonPressed = true;
+  else if (pressedInMemory / (double)arrayLength <= 0.1)
+    buttonPressed = false;
+  else
+	buttonPressed = wasPressed; // no change
+
+  if (buttonPressed != wasPressed) {
+    if (buttonPressed) {
+	  // only toggle on press start, not press end
+	  // TODO: write isOn to EEPROM so the state doesn't change if power is interrupted.
+      isOn = !isOn;
+	}
+
+    wasPressed = buttonPressed;
+  }
+  
+  return isOn;
+}
+
+double getChosenSpeed() {
+  int _voltage = analogRead(dialPin);
+  //double voltage = _voltage / 1023. * 5;
+  //Serial.println(voltage);
+  double speed = _voltage / 1023.0;
+  return speed;
+}
+
+void setSpeed(double speed) {
+  // output more ON means the transistor will be more on,
+  // and that will ground the control and reduce the fan speed.
+  int output = 255 * (1-speed);
+  analogWrite(outputPin, output);
+}
+
+void loop() {
+  bool on = getToggleState();
+  if (on) {
+    digitalWrite(powerPin, HIGH);
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(powerPin, LOW);
+    digitalWrite(ledPin, LOW);
+    return;
+  }
+  
+  double speed = getChosenSpeed();
+  setSpeed(speed);
+}
+
+// vim: sw=2:
